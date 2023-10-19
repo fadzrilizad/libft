b6d1e7a43cf26ff4fc7da3e199e0d3e7a6ecf3fa
Fadzril Izad <32401964+fadzrilizad@users.noreply.github.com>
2023-10-19 20:35:44 +0800

    strncmp strrchr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fizad <fizad@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 18:10:10 by fizad             #+#    #+#             */
-/*   Updated: 2023/10/19 18:10:12 by fizad            ###   ########.fr       */
+/*   Created: 2023/10/19 19:16:44 by fizad             #+#    #+#             */
+/*   Updated: 2023/10/19 19:16:45 by fizad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	len;
+	size_t	i;
 
-	len = ft_strlen(s);
-	if (s == NULL)
-		return (NULL);
-	while (len > 0)
-	{
-		len--;
-		if (s[len] == (char)c)
-			return ((char *)s + len);
-	}
-	if (*s == (char)c)
-		return ((char *)s);
-	return (NULL);
+	if (s1 == NULL || s2 == NULL || n == 0)
+		return (0);
+	i = 0;
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((int)(unsigned char)s1[i] - (int)(unsigned char)s2[i]);
 }
